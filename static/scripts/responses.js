d674a09c759306c9ace333565543217c862a673c
Keontse Raboloko <BIDA20-007@thuto.bac.ac.bw>
2022-05-31 16:06:24 -0700

    edited static/ file

---
+++ static/scripts/responses.js
@@ -0,0 +1,97 @@
+function getBotResponse(input) {
+	
+	// responses greetings
+    if (input == "hey") {
+        return "Hello there!";
+    
+    }
+	
+    if (input == "hi") {
+        return "Hello there! ";
+   
+    }
+	
+	
+	if (input == "sup") {
+        return "Hello there!"
+	
+	
+	}
+	if (input == "hello") {
+        return "Hello there!"
+	
+	
+	}
+	if (input == "hello!") {
+        return "Hello there!"
+	
+	
+	}
+	if (input == "heyo") {
+        return "Hello there!"
+	}	
+	
+	if (input == "good") {
+        return "how can i help you.you may ask questions related to the page! ";
+		
+	}
+	if (input == "im good") {
+        return "how can i help you.you may ask questions related to the page! ";
+	
+	}
+		if (input == "fine") {
+        return "how can i help you.you may ask questions related to the page! ";
+		
+	}
+	if (input == "im fine") {
+        return "how can i help you.you may ask questions related to the page! ";
+	}
+	if (input == "how are you") {
+        return "I\'m doing fine, and you?"
+	
+	}
+	
+	if (input == "thanks") {
+        return "you are welcome"
+	
+	}
+	if (input == "what are proteins") {
+        return "Proteins are large, complex molecules that play many critical roles in the body. They do most of the work in cells and are required for the structure, function, and regulation of the body's tissues and organs"
+	
+	}
+	if (input == "what are carbohydrates") {
+        return "Carbohydrates, or carbs, are sugar molecules. Along with proteins and fats, carbohydrates are one of three main nutrients found in foods and drinks. Your body breaks down carbohydrates into glucose. Glucose, or blood sugar, is the main source of energy for your body's cells, tissues, and organs."
+	
+	}
+	if (input == "what are fats") {
+        return "Fats in food come in several forms, including saturated, monounsaturated, and polyunsaturated. Too much fat or too much of the wrong type of fat can be unhealthy. Some examples of foods that contain fats are butter, oil, nuts, meat, fish, and some dairy products"
+	
+	}
+	if (input == "what are calories") {
+        return "A calorie is a measurement, just like a teaspoon or an inch. Calories are the amount of energy released when your body breaks down (digests and absorbs) food. The more calories a food has, the more energy it can provide to your body"
+	
+	}
+	
+	
+	
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+
+	
+	
+}
